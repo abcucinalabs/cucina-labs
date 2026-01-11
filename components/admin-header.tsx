095e7e2d2df156f39eed9bffff2880af23543574
Jiliohan <jim.iliohan@gmail.com>
2026-01-11 17:11:43 -0500

    Remove admin header title

--- components/admin-header.tsx
+++ components/admin-header.tsx
@@ -4,9 +4,6 @@ export function AdminHeader({ email }: { email: string }) {
   return (
     <header className="h-16 border-b border-[var(--border-default)] bg-white/80 backdrop-blur-xl">
       <div className="h-full flex items-center px-12">
-        <h1 className="text-xl text-black">
-          cucina <span className="font-bold">labs</span>
-        </h1>
       </div>
     </header>
   )
